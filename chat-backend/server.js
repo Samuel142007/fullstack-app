c22a785af18093b39d5e75f7cf79000579505819
Samuel142007 <samuelsaxxx@gmail.com>
2025-06-05 23:35:44 +0100

    Fix port binding for Render

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -77,7 +77,7 @@ io.on("connection", (socket) => {
 
 app.use(express.static("client/dist"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
