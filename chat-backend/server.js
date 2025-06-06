9ab483706414d238f58f7c15812a4448c8d91499
Samuel142007 <samuelsaxxx@gmail.com>
2025-06-06 12:00:48 +0100

    Update server.js

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -74,6 +74,7 @@ app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("/render-health", (req, res) => res.status(200).send("OK"));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
